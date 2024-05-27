72c78db3ead4f2c53a906b6aa54b425489d346b1
sarahgomes12 <loren4lima@gmail.com>
2024-05-27 19:53:55 -0300

    adicionando arquivos dev e css

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+    createCanvas(600, 600);
+    background("black");
+  }
+  
+  function draw() {
+      stroke("blue");
+      fill("red");
+    
+    if("mouseIspressed") {
+       rect(mouseX, mouseY, 20, 35);
+      
+       }
+    
+  }
+    
